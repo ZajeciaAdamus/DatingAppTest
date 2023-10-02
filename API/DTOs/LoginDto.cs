e9986fde81f042cdce301a2c99e7a0375bd66dec
Paweł Adamus <zajecia.adamus@gmail.com>
2023-10-02 14:13:41 +0200

    end of authorization

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,6 @@
+﻿public class LoginDto
+{
+    // wystawianie do API danych
+    public string Username {get; set;}
+    public string Password {get; set;}
+}
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+public class RegisterDto
+{
+    // wystawianie do API danych
+    [Required]
+    public string Username {get; set;}
+    [Required]
+    public string Password {get; set;}
+}
