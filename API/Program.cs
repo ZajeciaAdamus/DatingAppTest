a1b84051e0542e6e1572697b4c829c944fcd6a82
Paweł Adamus <zajecia.adamus@gmail.com>
2023-10-02 09:08:31 +0200

    end of debug

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(opt => 
@@ -13,6 +15,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
